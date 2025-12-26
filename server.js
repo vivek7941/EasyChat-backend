71d0133f54810ea1ba4c81305911986b13ee8db6
Vivek Yadav <142565389+vivek7941@users.noreply.github.com>
2025-12-26 22:10:49 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,46 +1,46 @@
-import express from "express";
-import "dotenv/config";
-import cors from "cors";
-import mongoose from "mongoose";
-import chatRoutes from "./routes/chat.js";
-
-const app = express();
-
-const PORT = 8080; 
-
-app.use(express.json());
-app.use(cors({
-  origin: "https://easy-chat-frontend-blush.vercel.app", 
-  methods: ["GET", "POST", "DELETE", "PUT"],
-  credentials: true
-}));
-
-
-// Health check route (useful for testing)
-app.get("/", (req, res) => res.send("API is running..."));
-
-app.use("/api/chat", chatRoutes);
-
-// 2. Database Connection Logic
-const connectDB = async () => {
-    try {
-        await mongoose.connect(process.env.MONGODB_URI);
-        console.log(" Connected to MongoDB successfully.");
-        
-        // 3. Only start the server once DB is connected
-        app.listen(PORT, () => {
-            console.log(` Server is running on http://localhost:${PORT}`);
-
-        });
-    } catch (error) { 
-        console.error(" Error connecting to MongoDB:", error.message);
-        process.exit(1); // Exit if DB fails
-    }
-};
-
-const GEMINI_API_KEY = process.env.GEMINI_API_KEY;
-if (!GEMINI_API_KEY) {
-  console.warn("⚠️ Warning: GEMINI_API_KEY is not set in environment.");
-}
-
-connectDB();
+  import express from "express";
+  import "dotenv/config";
+  import cors from "cors";
+  import mongoose from "mongoose";
+  import chatRoutes from "./routes/chat.js";
+  
+  const app = express();
+  
+  const PORT = 8080; 
+  
+  app.use(express.json());
+  app.use(cors({
+    origin: "https://easy-chat-frontend-blush.vercel.app", 
+    methods: ["GET", "POST", "DELETE", "PUT"],
+    credentials: true
+  }));
+  
+  
+  // Health check route (useful for testing)
+  app.get("/", (req, res) => res.send("API is running..."));
+  
+  app.use("/api/chat", chatRoutes);
+  
+  // 2. Database Connection Logic
+  const connectDB = async () => {
+      try {
+          await mongoose.connect(process.env.MONGODB_URI);
+          console.log(" Connected to MongoDB successfully.");
+          
+          // 3. Only start the server once DB is connected
+          app.listen(PORT, () => {
+              console.log(` Server is running on http://easychat-backend-1qag.onrender.com`);
+  
+          });
+      } catch (error) { 
+          console.error(" Error connecting to MongoDB:", error.message);
+          process.exit(1); // Exit if DB fails
+      }
+  };
+  
+  const GEMINI_API_KEY = process.env.GEMINI_API_KEY;
+  if (!GEMINI_API_KEY) {
+    console.warn("⚠️ Warning: GEMINI_API_KEY is not set in environment.");
+  }
+  
+  connectDB();

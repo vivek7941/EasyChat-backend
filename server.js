b7a0ea184e83458355b74fed4d570c0b3c4a98dd
Vivek Yadav <142565389+vivek7941@users.noreply.github.com>
2025-12-27 13:14:02 +0530

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 8080;
 
 app.use(express.json());
 app.use(cors({
-  origin: "https://easychat-zeta.vercel.app/", 
+  origin: "https://easychat-ochre.vercel.app/", 
   methods: ["GET", "POST", "DELETE", "PUT"],
   credentials: true
 }));

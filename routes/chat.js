619daabce67a02926051dabfd4f22aadec9af5f0
Vivek <142565389+vivek7941@users.noreply.github.com>
2026-04-14 20:12:10 +0530

    Update chat.js

--- routes/chat.js
+++ routes/chat.js
@@ -21,7 +21,9 @@ async function getGeminiAPIResponse(messages) {
     }));
 
     
-   const url = `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=${GEMINI_API_KEY}`;
+  
+
+    const url = `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${GEMINI_API_KEY}`;
     
     const options = {
         method: "POST",
